14dcbdc1c94d48b0ce8e5e26500352441e7772d2
zhiyao ni <nizhiyaooooo@gmail.com>
2022-11-21 11:46:40 +0800

    app

--- Gemfile
+++ Gemfile
@@ -4,6 +4,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 
 gem 'rails-controller-testing', group: [ :test ]
+gem "autoprefixer-rails"
+gem "bootstrap"
+gem "font-awesome-sass", "~> 6.1"
+gem "simple_form", github: "heartcombo/simple_form"
+gem "sassc-rails"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
+pin_all_from "app/javascript/components", under: "components"
